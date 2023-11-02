71b5f432a58c642574e925ec1dad170ea561ffaa
head1ton <head1ton@gmail.com>
2023-11-02 12:13:41 +0900

    init

---
+++ src/main/java/ai/iamportpractice/IamportPracticeApplication.java
@@ -0,0 +1,13 @@
+package ai.iamportpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamportPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamportPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/iamportpractice/IamportPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.iamportpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamportPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
